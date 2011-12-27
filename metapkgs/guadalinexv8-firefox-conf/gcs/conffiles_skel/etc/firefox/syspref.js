2f8699784f5468dfc3d3eb4e68b54f3bdfa6c287
stewie <stewie@a73ce567-8c85-45d1-a140-bc817bc41e4b>
2011-12-27 14:16:48 +0000

    created metapkg to customize firefox

---
+++ metapkgs/guadalinexv8-firefox-conf/gcs/conffiles_skel/etc/firefox/syspref.js
@@ -0,0 +1,16 @@
+// This file can be used to configure global preferences for Firefox
+// Example: Homepage
+
+
+
+//Configura homepage
+user_pref("browser.startup.homepage", "http://www.guadalinex.org/");
+//Desactiva la web de bienvenida de Firefox
+user_pref("startup.homepage_welcome_url", "");
+//Desactiva la ventana de "Conoce tus derechos" 
+user_pref("browser.rights.3.shown", true);
+
+
+
+//user_pref("distribution.canonical.bookmarksProcessed", true);
+//user_pref("browser.bookmarks.restore_default_bookmarks", false);
